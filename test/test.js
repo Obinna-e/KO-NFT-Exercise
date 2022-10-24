4501802e59ca27575f3d16550faab32dfaac50bd
Obinna Ezekwem <obinnaezekwem@yahoo.com>
2022-10-24 14:59:30 +0100

    test adjusted

--- test/test.js
+++ test/test.js
@@ -1,14 +1,31 @@
 const {expect} = require("chai");
 const { hexStripZeros } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
 const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 describe("KnownNft", function() {
+    
+    this.beforeEach(async function() {
+        const KnownNft = await ethers.getContractFactory("KnownNft");
+         knownNft = await KnownNft.deploy();
+    });
+
     it("Should return the right name and symbol", async function() {
-        const KnownNft = await hre.ethers.getContractFactory("KnownNft");
+       
+        expect(await knownNft.name()).to.equal("KnownNft");
+        expect(await knownNft.symbol()).to.equal("KNWN");
+    });
+
+    it("Should only mint 3 nfts for 0.01 ether", async function() {
+        const [owner, addr1, addr2] = await ethers.getSigners();
+
+        const KnownNft = await ethers.getContractFactory("KnownNft");
         const knownNft = await KnownNft.deploy();
 
         await knownNft.deployed();
-        expect(await knownNft.name()).to.equal("KnownNft");
-        expect(await knownNft.symbol()).to.equal("KNWN");
+        
+
+        knownNft.connect(addr1).publicMint();
+        expect(await knownNft.balanceOf(addr1.address)).to.equal('0');
     });
 })
