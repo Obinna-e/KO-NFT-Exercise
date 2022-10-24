544331529ea1162d4117bda1ad1b54b714c7ada0
Obinna Ezekwem <obinnaezekwem@yahoo.com>
2022-10-24 16:46:32 +0100

    deployer script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+  Georli: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
+    },
+  }
+  
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const KnownNft = await ethers.getContractFactory("KnownNft");
+    const gasPrice = await KnownNft.signer.getGasPrice();
+    console.log(`Current gas price: ${gasPrice}`);
+
+    const estimatedGas = await KnownNft.signer.estimateGas(
+        KnownNft.getDeployTransaction()
+    );
+
+    console.log(`Estimated gas:${estimatedGas}`);
+
+    const deploymentPrice = gasPrice.mul(estimatedGas);
+    const deployerBalance = await KnownNft.signer.getBalance();
+
+    console.log(`Deployer balance: ${ethers.utils.formatEther(deployerBalance)}`);
+
+    if (Number(deployerBalance) < (Number(deploymentPrice) / 1000000000)) {
+        throw new Error("You dont have enough balance to deploy.");
+     }
+    //  // Start deployment, returning a promise that resolves to a contract object
+     const myNFT = await KnownNft.deploy();
+     await myNFT.deployed();
+     console.log("Contract deployed to address:", myNFT.address);
+     }
+     main().then(() => process.exit(0)).catch((error) => {
+     console.error("Error:", error);
+     process.exit(1);
+     });
+
