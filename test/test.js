c120565bc18b9ad6d8d96e9bdc326a9d1e333f70
Obinna Ezekwem <obinnaezekwem@yahoo.com>
2022-10-24 14:39:55 +0100

    basic test setup

--- test/test.js
+++ test/test.js
@@ -1,7 +1,14 @@
-require("@nomiclabs/hardhat-ethers");
-
 const {expect} = require("chai");
+const { hexStripZeros } = require("ethers/lib/utils");
+const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 describe("KnownNft", function() {
-    
+    it("Should return the right name and symbol", async function() {
+        const KnownNft = await hre.ethers.getContractFactory("KnownNft");
+        const knownNft = await KnownNft.deploy();
+
+        await knownNft.deployed();
+        expect(await knownNft.name()).to.equal("KnownNft");
+        expect(await knownNft.symbol()).to.equal("KNWN");
+    });
 })
